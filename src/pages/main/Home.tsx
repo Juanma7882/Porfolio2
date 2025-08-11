8ff4ebbe8fd1c1222c5964340139c31d0e3fd515
Juanma7882 <juanmaf236@gmail.com>
2025-08-11 17:55:23 -0300

    cv en ambos idiomas

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -99,7 +99,7 @@ function Home() {
               {/* Botón de CV */}
 
               <a
-                href="/cv-juan-fernandez.pdf" // reemplazalo con la ruta correcta del archivo en tu proyecto
+                href="cv/Fernandez Juan Manuel.pdf" 
                 download
                 className="mt-5 py-2 px-6 rounded-lg flex items-center gap-2 text-sm font-semibold
              bg-white text-black border border-gray-300 hover:border-gray-500 hover:bg-gray-100 transition-colors
